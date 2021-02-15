72ff3911a37a9c79538e73c7adf46ebd763e4086
Guy Lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2021-02-15 11:54:02 -0800

    finished dummy sliding nav

--- resources/script/side-nav.js
+++ resources/script/side-nav.js
@@ -1,9 +1,12 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("nav").style.left = "0px";
+    document.querySelector(".nav-open-btn").style.opacity = "0";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("nav").style.left = "-250px";
+    document.querySelector(".nav-open-btn").style.opacity = "1";
   }
+  
