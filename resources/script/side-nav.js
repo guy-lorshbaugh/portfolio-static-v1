697e04428bc472979034ec6c263768de1da7816f
Guy Lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2021-02-15 15:51:33 -0800

    improved menu interactivity

--- resources/script/side-nav.js
+++ resources/script/side-nav.js
@@ -1,12 +1,19 @@
-/* Set the width of the side navigation to 250px */
+var navMenu = document.getElementById('nav')
+
 function openNav() {
-    document.getElementById("nav").style.left = "0px";
-    document.querySelector(".nav-open-btn").style.opacity = "0";
+  navMenu.style.left = "0px";
+  document.querySelector(".nav-open-btn").style.opacity = "0";
+}
+
+function closeNav() {
+  navMenu.style.left = "-260px";
+  document.querySelector(".nav-open-btn").style.opacity = "1";
+}
+
+document.addEventListener('mousedown', function(event){
+  if(navMenu.style.left = "0px"){
+          if(!navMenu.contains(event.target)){
+                  closeNav();
+          }
   }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("nav").style.left = "-250px";
-    document.querySelector(".nav-open-btn").style.opacity = "1";
-  }
-  
+})
